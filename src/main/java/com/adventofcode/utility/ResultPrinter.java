ed48662888858dcc46bf67ca512767f79b9f759a
luca.perez <luca.perez@studenti.unimi.it>
2025-03-29 21:51:52 +0100

    Aggiunti metodi della classe ResultPrinter

--- src/main/java/com/adventofcode/utility/ResultPrinter.java
+++ src/main/java/com/adventofcode/utility/ResultPrinter.java
@@ -1,4 +1,11 @@
 package com.adventofcode.utility;
 
 public class ResultPrinter {
+    public static void part1(int result) {
+        System.out.println("Il risultato della prima parte è:" + result);
+    }
+
+    public static void part2(int result) {
+        System.out.println("Il risultato della seconda parte è:" + result);
+    }
 }
