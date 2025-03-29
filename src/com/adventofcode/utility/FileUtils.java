392dd6706cbabbd59b6a9bcad976d324cef26fd0
luca.perez <luca.perez@studenti.unimi.it>
2025-03-29 21:38:19 +0100

    Sistemata struttura, aggiunta classe Utils per lettura input. Aggiunto primo test per questo metodo.

--- src/com/adventofcode/utility/FileUtils.java
+++
@@ -1,4 +0,0 @@
-package com.adventofcode.utility;
-
-public class FileUtils {
-}
---
+++ src/main/java/com/adventofcode/utility/FileUtils.java
@@ -0,0 +1,29 @@
+package com.adventofcode.utility;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+import java.nio.file.Files;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class FileUtils {
+    public static List<String> readInputAsStringList(String filePath) throws IOException {
+        Objects.requireNonNull(filePath, "Path cannot be null");
+        return Files.readAllLines(Paths.get(filePath));
+    }
+
+    public static List<Integer> readInputAsIntList(String filePath) throws IOException {
+        Objects.requireNonNull(filePath, "Path cannot be null");
+        return Files.lines(Paths.get(filePath))
+                .map(Integer :: parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public static int[] readInputAsArrayOfInt(String filePath) throws IOException {
+        Objects.requireNonNull(filePath, "Path cannot be null");
+        return Files.lines(Paths.get(filePath))
+                .mapToInt(Integer :: parseInt)
+                .toArray();
+    }
+}
